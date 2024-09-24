1eb9bcb943bd37524eb68d661dc58c3c0e1c826f
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2024-09-24 12:26:42 +0200

    Exercices 24-09-24

--- starter/script00.js
+++ starter/script00.js
@@ -1,1 +1,20 @@
 console.log("Hello, World !");
+
+let myVar = 'Bonjour';
+let myNumber = 1;
+let myBool = false;
+let myBool2 = true;
+
+
+myVar = 'TOTO';
+myNumber = 2;
+
+const myConst = 'Hello';
+
+console.log(typeof myVar);
+
+alert('Bonjour');
+
+const myAge = prompt("Quel est ton âge ?");
+
+console.log(myAge);
