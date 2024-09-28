015d415760bbac7a31aa9b5efc7599147605cc39
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2024-09-28 12:07:18 +0200

    Exercices 24-09-24 v6

--- starter/script011.js
+++ starter/script011.js
@@ -4,16 +4,15 @@
 
 // Opérateurs multiples
 // 1. Utilisez les opérateurs +, *, et / pour effectuer une opération mathématique complexe. Affichez le résultat.
-console.log(`Le résultat de l'opération de ces opérateurs multiples est ${2 + 5 / 4 * 2} ! Le calcul pour obtenir ce résultat est de 2 + 5 / 4 * 2.`);
+console.log(`Le résultat de cette opération mathématique complexe est ${2 + 5 / 4 * 2} ! Le calcul pour obtenir ce résultat est de 2 + 5 / 4 * 2.`);
 
 // Groupement
 // 2. Créez une expression mathématique qui nécessite le groupement des opérations. Utilisez des parenthèses pour clarifier l'ordre des opérations. Affichez le résultat.
+console.log(`Le résultat de l'opération de ce groupement d'opérations est ${5((2 + 5) / (4 * 2))} ! Le calcul pour obtenir ce résultat est de 5((2 + 5) / (4 * 2)).`);
 
 // Affectations multiples
 // 3. Déclarez deux variables x et y. Utilisez des affectations multiples pour leur donner des valeurs. Par exemple, x = 5 et y = 10. Affichez les valeurs de x et y.
-let x, y;
-x = 5;
-y = 10;
+let x = 5, y = 10;
 
 console.log(`Les valeurs x et y sont ${x} et ${y}.`);
 
